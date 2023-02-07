3c4a61d9f715a053fdb6b555446c14cce2e08bb5
amanyadav2030 <amanyadav919821@gmail.com>
2023-02-07 16:25:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const dbConnect = require('./config/db');
+const {ContentRouter,UserRouter} = require('./routes/index.js');
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use('/content',ContentRouter);
+app.use('/user',UserRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello from NFT Backend');
+});
+
+app.listen(port,async()=>{
+    await dbConnect();
+    console.log(`server is running on port ${port}`)
+})
